10d1687bd1c431ec871764431a812611626f4742
cluelesselectrostar <ytc19@ic.ac.uk>
2020-01-16 15:48:48 +0000

    wk1T3PreReFactor

---
+++ lab1/test_drawing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+g++ house.cpp -o house
+g++ spiral.cpp -o spiral
+
+./house > house.got.svg
+./spiral > spiral.got.svg
+
+diff house.got.svg house.ref.svg
+diff spiral.ref.svg
+
+echo "Success"
